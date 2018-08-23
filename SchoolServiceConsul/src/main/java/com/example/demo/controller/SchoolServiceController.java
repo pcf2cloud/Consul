2a5fc7e6f9c16427d142dfcaeb2e08490fa0ace5
PCF <40490811+pcf2cloud@users.noreply.github.com>
2018-08-23 21:10:14 +0530

    Add files via upload
    cousul

---
+++ SchoolServiceConsul/src/main/java/com/example/demo/controller/SchoolServiceController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+ 
+@RestController
+public class SchoolServiceController {
+	
+	@Autowired
+    StudentServiceDelegate studentServiceDelegate;
+ 
+    @RequestMapping(value = "/getSchoolDetails/{schoolname}", method = RequestMethod.GET)
+    public String getStudents(@PathVariable String schoolname)
+    {
+        System.out.println("Going to call student service to get data!");
+        return studentServiceDelegate.callStudentServiceAndGetData(schoolname);
+    }
+}
